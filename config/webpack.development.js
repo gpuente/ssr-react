6345a91e3497901f3c5c9a469c0159ab2f4d7f59
Guillermo Puente <guillermo.ps09@gmail.com>
2017-12-17 22:33:10 -0300

    Added chunkhash for production

--- config/webpack.development.js
+++ config/webpack.development.js
@@ -4,6 +4,7 @@ const parts = require('./webpack.parts');
 /******************* Development config *********************/
 
 const developmentConfig = merge([
+  parts.output('hash'),
   parts.devTools(),
   parts.devServer({
     host: process.env.HOST || '0.0.0.0',
